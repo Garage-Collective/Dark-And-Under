0bb757142fc174bac986788889e6a1c508c2c0bd
Pharap <2933055+Pharap@users.noreply.github.com>
2018-03-24 23:13:38 +0000

    Delete Item.cpp

--- src/entities/Item.cpp
+++
@@ -1,9 +0,0 @@
-#include "Item.h"
-
-Item::Item() : Base() {}
-
-ItemType Item::getItemType() const                { return (ItemType)(_flags & 0x7F); }
-bool Item::getEnabled() const                     { return ((_flags & 0x80) > 0); }
-
-void Item::setItemType(const ItemType value)      { _flags = (_flags & 0x80) | ((uint8_t)value); }
-void Item::setEnabled(const bool value)           { _flags = (_flags & 0x7F) | (value ? 0x80 : 00); }
