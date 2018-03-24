f301ffa2f927a2279a6a8bbfa12bc3ff0e81e337
Pharap <2933055+Pharap@users.noreply.github.com>
2018-03-24 23:09:59 +0000

    Delete Images.h

--- images/Images.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Images_Base.h"
-#include "Images_Dungeons.h"
-#include "Images_Enemies.h"
-#include "Images_Fight.h"
-#include "Images_Inventory.h"
-#include "Images_Items.h"
-#include "Images_Map.h"
